226279dc308e78afe2a7bff4431d18ba11fa72c9
Ravipatelai <rkpg6331@gmail.com>
2026-05-13 20:26:31 +0530

    first commit

--- client/src/VideoCall.jsx
+++ client/src/VideoCall.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://videocall-production-98fb.up.railway.app/");
 
 const servers = {
   iceServers: [
